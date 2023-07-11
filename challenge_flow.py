bb582cd722978b5f7741a9685776588ea057a9b2
TechieTeee <techieteee@protonmail.com>
2023-07-11 19:07:32 +0000

    Added transformation tasks for unique values and missing values

--- challenge_flow.py
+++ challenge_flow.py
@@ -27,12 +27,30 @@ def transform(data: dict) -> pd.DataFrame:
     return pd.DataFrame(transformed)
 
 @task
+def filter_missing_values(data: dict) -> dict:
+    filtered = [item for item in data if all (key in item for key in ('userId', 'id', 'title', 'body'))]
+    return filtered
+
+@task
+def find_unique_posts(data: dict) -> list:
+    unique_posts = []
+    post_ids = set()
+    for item in data:
+        post_id = item['id']
+        if post_id not in post_ids:
+            post_ids.add(post_id)
+            unique_posts.append(item)
+return unique_posts
+
+@task
 def load(data: pd.DataFrame, path: str) -> None:
     data.to_parquet(path, index=False)
 
 with Flow("DataProcessingFlow") as flow:
     posts = extract(url='https://jsonplaceholder.typicode.com/posts')
-    df_posts = transform(posts)
+    filtered_posts = filter_missing_values(posts)
+    unique_posts = find_unique_posts(filtered_posts)
+    df_posts = transform(filtered_posts)
     load(data=df_posts, path=f'data/posts_{int(datetime.now().timestamp())}.parquet')
 
 flow.run()
