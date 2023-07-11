1680950924129b5efb6842223449f4614a0a602b
TechieTeee <techieteee@protonmail.com>
2023-07-11 18:33:46 +0000

    Added task for converting data from json to parquet

--- challenge_flow.py
+++ challenge_flow.py
@@ -1,19 +1,38 @@
-from prefect import Flow, task
+import json
 import requests
+import pandas as pd
+from datetime import datetime
+from prefect import task, Flow
+
 
 @task
-def fetch_data():
-    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+def extract(url: str) -> dict:
+    response = requests.get(url)
     if response.status_code == 200:
-        return response,json()
+        return json.loads(response.content)
     else:
         raise Exception('Failed to successfully fetch data')
 
-def data_processing_flow():
-    with Flow("DataProcessingFlow") as flow:
-        data = fetch_data
-    return flow
 
+@task
+def transform(data: dict) -> pd.DataFrame:
+    transformed = []
+    for item in data:
+        transformed.append({
+            'UserId': item['userId'],
+            'Id': item['id'],
+            'Title': item['title'],
+            'Body': item['body']
+        })
+    return pd.DataFrame(transformed)
 
-flow = data_processing_flow()
+@task
+def load(data: pd.DataFrame, path: str) -> None:
+    data.to_parquet(path, index=False)
+
+with Flow("DataProcessingFlow") as flow:
+    posts = extract(url='https://jsonplaceholder.typicode.com/posts')
+    df_posts = transform(posts)
+    load(data=df_posts, path=f'data/posts_{int(datetime.now().timestamp())}.parquet')
+
 flow.run()
